0e1afaa5e42560d8403d6420eb7ebfb6b0ec9f3a
Diana Utan <didiutzi@gmail.com>
2021-05-18 16:53:47 -0500

    read file using File System module

--- demo_module.js
+++ demo_module.js
@@ -1,13 +1,12 @@
 var http = require("http");
-var dt = require("./myfirstmodule");
+// var dt = require("./myfirstmodule");
+var url = require("url");
 
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("The date and time is currently: " + dt.myDateTime());
-    res.end();
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.end(txt);
   })
   .listen(8080);
-
-
-  
