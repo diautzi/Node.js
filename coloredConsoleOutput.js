73e2350c2ec35407ff886a913d3dbb4031a54456
Diana Utan <didiutzi@gmail.com>
2021-05-20 22:35:24 -0500

    used Progress package to create progress bar

--- coloredConsoleOutput.js
+++ coloredConsoleOutput.js
@@ -1,4 +1,16 @@
+// use Chalk libary to color text output in console
 const chalk = require("chalk");
 console.log(chalk.yellow("hi!"));
 console.log(chalk.red("diana!"));
 console.log(chalk.blue("helloWorld!"));
+
+// create progreess bar using Progress package
+const ProgressBar = require("progress");
+
+const bar = new ProgressBar(":bar", { total: 10 });
+const timer = setInterval(() => {
+  bar.tick();
+  if (bar.complete) {
+    clearInterval(timer);
+  }
+}, 100);
