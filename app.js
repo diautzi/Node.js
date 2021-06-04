f558a36d51927dcd2e742bf09838c9849461e091
Diana Utan <didiutzi@gmail.com>
2021-06-03 19:07:09 -0500

    different module patterns in node.js

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ const greet = require("./greet");
 
 greet.english();
 greet.romanian();
+
+var greet2 = require("./greet/greet");
+greet2.greet();
+greet2.greeting = "Changed hello world";
+
+var greet2b = require("./greet/greet");
+greet2b.greet();
+
+var Greet3 = require("./greet/greet2");
+var greeter = new Greet3();
+greeter.greet();
+
+var greet4 = require("./greet/greet3").greet;
+
+greet4();
